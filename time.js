31e8bda3a428df6dbda92db3134ccc81fe6aa447
Yahya <yeahyes766@gmail.com>
2024-05-08 14:09:31 +0300

    added at 209

---
+++ time.js
@@ -0,0 +1,16 @@
+
+const date = new Date();
+
+let year = date.getFullYear();
+let month = date.getMonth();
+let day = date.getDate();
+let hour = date.getHours();
+let minute = date.getMinutes();
+let second = date.getSeconds();
+
+document.write(year);
+document.write(month);
+document.write(day);
+document.write(hour);
+document.write(minute);
+document.write(second); 
